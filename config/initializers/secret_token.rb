b62ea81f783514210fe2ef1a4027b711273d7a71
Audrius Brazauskas <audriustbrazauskas@gmail.com>
2015-11-02 19:28:51 +0200

    new site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audriausgimtadienis::Application.config.secret_key_base = '601fde1ded91a8b92ce0ec9a14438ecaa1768279c980e3633a100a07d544db58a80d55f58cc93e3122732bd33b4d391501b52f51d6362bd80c8a2299324db4ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Audriausgimtadienis::Application.config.session_store :cookie_store, key: '_audriausgimtadienis_session'
